ecc8d361bcd427b88d0780db1b6457b82fceff38
AKK <kadir2257@gmail.com>
2023-03-08 22:29:13 +0300

    ColorChangeButtuonTest

---
+++ changeBackColor/change_background_color/backgroundcolorchange/lib/main3.dart
@@ -0,0 +1,35 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Color _backgroundColor = Colors.white;
+
+  void _changeBackgroundColor() {
+    setState(() {
+      _backgroundColor = Color((Random().nextDouble() * 0xFFFFFF).toInt() << 0).withOpacity(1.0);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: _backgroundColor,
+        body: Center(
+          child: ElevatedButton(
+            child: Text('Change Background Color'),
+            onPressed: _changeBackgroundColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
