8ae9fc586a3a28a71f66f78e9ae3f73be4583dbf
AKK <kadir2257@gmail.com>
2023-03-21 21:41:52 +0300

    yorum satiri eklendi

--- changeBackColor/change_background_color/backgroundcolorchange/lib/main3.dart
+++ changeBackColor/change_background_color/backgroundcolorchange/lib/main3.dart
@@ -25,7 +25,7 @@ class _MyAppState extends State<MyApp> {
         backgroundColor: _backgroundColor,
         body: Center(
           child: ElevatedButton(
-            child: Text('Change Backgroundbbbbbbb Color'),
+            child: Text('Change Backgroundbbbbbbb Color'),//asdasdasdasdasdasdasd
             onPressed: _changeBackgroundColor,
           ),
         ),
