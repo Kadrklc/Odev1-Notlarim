73a15c0dd13a97a75d7987c395c7aa325bb95a6b
AKK <kadir2257@gmail.com>
2023-03-21 21:01:22 +0300

    arka plan renk degoistirme

--- changeBackColor/change_background_color/backgroundcolorchange/lib/main3.dart
+++ changeBackColor/change_background_color/backgroundcolorchange/lib/main3.dart
@@ -25,7 +25,7 @@ class _MyAppState extends State<MyApp> {
         backgroundColor: _backgroundColor,
         body: Center(
           child: ElevatedButton(
-            child: Text('Change Backgrounddd Color'),
+            child: Text('Change Background Color'),
             onPressed: _changeBackgroundColor,
           ),
         ),
