ad9ca854f78c2c0353974e66c24e4656f9af9c8a
AKK <kadir2257@gmail.com>
2023-03-21 21:31:41 +0300

    torum eklendi

---
+++ GOUA/class.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Ogrenci {
+  String ad;
+  int yas;
+  Ogrenci({
+    required this.ad,
+    required this.yas,
+  });
+  
+}
+
+void main(List<String> args) {}
